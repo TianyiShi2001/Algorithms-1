f0dce35dd7c770f2a0ac27aa47a6eab58a3a320a
Tianyi Shi <ShiTianyi2001@outlook.com>
2021-06-20 23:41:01 +0100

    Detailed explanation on GCD

--- src/main/java/com/williamfiset/algorithms/math/GCD.java
+++ src/main/java/com/williamfiset/algorithms/math/GCD.java
@@ -11,6 +11,17 @@ public class GCD {
 
   // Computes the Greatest Common Divisor (GCD) of a & b
   // This method ensures that the value returned is non negative
+  //
+  // Explanation: dividing a by b yields a quotient q and a remainder r,
+  // which is equal to (a - bq); i.e. a can be written as
+  // a = bq + r
+  // a = bq + (a - bq)
+  // let k be a common divisor of a and b, and divide the above equation by k:
+  // a/k = bq/k + (a - bq)/k
+  // Since a/k and bq/k are both integers, (a - bq)/k, which is r/k, must also
+  // be an integer, i.e. the common divisor of a and b must also be a divisor
+  // of r, the remainder of a/b.
+  // Accordingly, gcd(a, b) = gcd(b, r) = gcd(b, a % b)
   public static long gcd(long a, long b) {
     return b == 0 ? (a < 0 ? -a : a) : gcd(b, a % b);
   }
